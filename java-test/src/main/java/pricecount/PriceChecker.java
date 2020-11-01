6abf6c5305d871e886241d249a570d34d936b123
francois <fjougleux@gmail.com>
2020-11-01 17:31:02 +0100

    init

---
+++ java-test/src/main/java/pricecount/PriceChecker.java
@@ -0,0 +1,22 @@
+package pricecount;
+
+import java.util.List;
+
+public class PriceChecker {
+
+    /**
+     * This method returns number of Dates with at least n prices
+     *
+     * @param prices
+     * @return
+     */
+    public int checkPrices(List<PriceDto> prices, int n) {
+
+        //body to add
+        int count = 0;
+        //...
+
+        return count;
+
+    }
+}
---
+++ java-test/src/main/java/pricecount/PriceDto.java
@@ -0,0 +1,14 @@
+package pricecount;
+
+import java.util.Date;
+
+public class PriceDto {
+
+    private long productId;
+    private Date date;
+    private boolean isOfficial;
+    private double value;
+
+
+
+}
